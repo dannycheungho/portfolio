64fc9a1753aca73493717c10cdd7416a83ba5c3a
Danny Cheung <danny.cheung@pwhhouse.tech>
2023-02-17 13:26:35 +0800

    fix : image configure

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  }
 }
 
 module.exports = nextConfig
