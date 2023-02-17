195d19e4c1715111b66e4bb2a59bf0c7600699bd
Danny Cheung <danny.cheung@pwhhouse.tech>
2023-02-17 15:33:54 +0800

    fix : image configure 6

--- next.config.js
+++ next.config.js
@@ -1,21 +1,36 @@
-/** @type {import('next').NextConfig} */
-// next.config.js
+// /** @type {import('next').NextConfig} */
+// // next.config.js
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = ''
+// let assetPrefix = ''
+// let basePath = ''
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  //const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  //console.log(repo);
-  assetPrefix = `/portfolio/`
-  //basePath = `/${repo}`
-}
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+//   //console.log(repo);
+//   assetPrefix = `/portfolio/`
+//   //basePath = `/${repo}`
+// }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  reactStrictMode: true,
-  swcMinify: true
-}
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   reactStrictMode: true,
+//   swcMinify: true
+// }
+
+const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins');
+const isDev = process.env.NODE_ENV !== "production";
+
+module.exports = withPlugins([
+    [withImages, {
+        assetPrefix: isDev ? "": "/portfolio/"
+    }],
+    {
+        trailingSlash: true,
+        basePath: isDev ? "": "/portfolio",
+        assetPrefix: isDev ? "": "/portfolio/"    
+    }
+])
