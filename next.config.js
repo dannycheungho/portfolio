44800e93afc73a4d2fb00e4450ad4e92feb6f2f9
Danny Cheung <danny.cheung@pwhhouse.tech>
2023-02-17 15:23:40 +0800

    fix : image configure 5

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  //const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  //console.log(repo);
+  assetPrefix = `/portfolio/`
+  //basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: assetPrefix,
+  reactStrictMode: true,
+  swcMinify: true
+}
